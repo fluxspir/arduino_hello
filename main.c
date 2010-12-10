067f934b2777e765581c6892d001e6a684bfa092
Franck Labadille <franck@kernlog.net>
2010-12-11 00:23:17 +0100

    tentative "actuelle" foireuse

--- main.c
+++ main.c
@@ -8,32 +8,12 @@ setup_atmega328p_serial(serial *port)
 		TXEN0, RXCIE0, UDRE0, U2X0);
 }
 
-void
-text_print_header(serial *port, char *name)
-{
-	serial_write(port, ':');
-	serial_print(port, name);
-	serial_write(port, ':');
-}
-
-void
-text_print(serial *port, char *txt)
-{
-	text_print_header(port, "first try headers");
-	serial_print(port, txt);
-	serial_write(port, '\n');
-}
-
 int main(void) {
-	uint8_t query;
-	/*double avg;*/
 	serial port;
 	setup_atmega328p_serial(&port) ;
 	init() ;
-	text[] = "Hello World!" ;
 	serial_begin(&port, 9600) ;
-	text_print(&port, text) ;
-//	serial_print(&port, "Hello World!") ;
+	serial_print(&port, "Hello World!") ;
 	serial_flush(&port) ;
 	serial_end(&port) ;
 	return 0 ;
